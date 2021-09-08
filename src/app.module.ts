f602cf23dfa146e36d0c41f5cba6a09ee9a0f992
kou <contact@awesome-linus.com>
2021-09-09 00:27:42 +0900

    add graphql setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path'; // <- 追加
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
